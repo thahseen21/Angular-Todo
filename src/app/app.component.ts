fbcb62769a3a49566b24c463b75fcc7b34ad7d08
thahseen21 <thahseen.niaz@gmail.com>
2023-06-17 13:22:29 +0530

    basic todo add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo';
+
+  newTodo: string = '';
+  todos: todo[] = [];
+
+  constructor() {
+    this.todos = [
+      { id: 1, name: 'test 1', isCompleted: false, isDeleted: false },
+      { id: 2, name: 'test 2', isCompleted: false, isDeleted: false },
+      { id: 3, name: 'test 3', isCompleted: false, isDeleted: false },
+    ];
+  }
+
+  addTodo() {
+    this.todos.push({
+      id: this.todos.length + 1,
+      name: this.newTodo,
+      isCompleted: false,
+      isDeleted: false,
+    });
+    this.newTodo = '';
+  }
+
+  removeTodoById(todoId: number) {
+    let selectedTodo = this.todos.find((todo) => todoId == todo.id);
+    if (selectedTodo) selectedTodo.isDeleted = true;
+
+    console.log(selectedTodo);
+  }
 }
+
+type todo = {
+  id: number;
+  name: string;
+  isCompleted: boolean;
+  isDeleted: boolean;
+};
